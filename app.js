f699b84f17c06d632c9732386bb9249745163345
moazam <moazamawan256@gmail.com>
2023-09-16 16:21:02 +0500

    ch 2 ass

---
+++ app.js
@@ -0,0 +1,12 @@
+var username
+var myname = " Moazam Ali"
+console.log(myname)
+var message = "Hello world"
+alert(message)
+var pizza= " pizza \n pizz \n piz \n pi \n p"
+console.log(pizza)
+var email = "My email address is example@example.com" 
+alert(email)
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+console.log(design)
+alert(design)
